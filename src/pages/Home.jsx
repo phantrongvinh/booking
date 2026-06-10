b9d3d88ea8ff8984d31126863f9e9ec491c4525c
PhanTrongVinh <1phantrongvinh98@gmail.com>
2026-06-10 14:47:42 +0700

    shadcn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
-  return <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>
+    </>
+  );
 };
 
 export default Home;
