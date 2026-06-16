de0c273df66121cf5415734371ff217a395bdfd0
phonghuynh031226 <phonghuynh031226@gmail.com>
2026-06-16 09:56:15 +0700

    forgotpassword, verifyotp

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,9 @@ import Home from "@/pages/Home";
 import MainLayout from "@/pages/layout/MainLayout";
 import Menu from "@/pages/Menu";
 import Notfound from "@/pages/Notfound";
+import ForgotPassword from "@/pages/ForgotPassword";
+import VerifyOtp from "@/pages/VerifyOtp";
+import OrderSuccessModal from "@/pages/OrderSuccessModal";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
       <Routes>
         {/* Không dùng layout */}
         <Route path="/auth" element={<Auth />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/verify-otp" element={<VerifyOtp />} />
+        <Route path="/order-success" element={<OrderSuccessModal />} />
 
         {/* Dùng layout */}
         <Route element={<MainLayout />}>
