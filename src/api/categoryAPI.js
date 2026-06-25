0dc9100d402c469a397dd035781fbbe6de983f4e
phantrongvinh <1phantrongvinh98@gmail.com>
2026-06-25 21:09:49 +0700

    Categories api + Products api + Auth api + home render

--- src/api/categoryAPI.js
+++ src/api/categoryAPI.js
@@ -1,1 +1,14 @@
+import axiosClient from "./axiosClient";
+
 const url = "/Categories";
+
+const fetchAllCategory = async () => {
+  const res = await axiosClient.get(`${url}`);
+  return res;
+};
+
+const categoryAPI = {
+  fetchAllCategory,
+};
+
+export default categoryAPI;
